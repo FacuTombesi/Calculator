2e4510f4d66150181cca46aa7ccd3a1720dcfdcb
FacuTombesi <facundomtombesi@gmail.com>
2023-06-07 23:22:53 -0300

    script.js update

--- script.js
+++ script.js
@@ -17,21 +17,52 @@ class Calculator { // Guarda la informacion de los numeros y operadores que se p
 
     // appendNumber() define lo que ocurre cada vez que se hace click en un numero
     appendNumber(number) {
-        if (number === "." && this.currentOperand.includes(".")) return; // Me fijo si quiero agregar y si ya tengo un "." en currentOperand. Si ya hay termino la operacion para que solo se pueda agregar un "." por operacion
+        if (number === "." && this.currentOperand.includes(".")) return; // Me fijo si quiero agregar y si ya tengo un "." en currentOperand. Si ya hay, termino la operacion para que solo se pueda agregar un "." por operacion
         this.currentOperand = this.currentOperand.toString() + number.toString(); // Necesito convertir los numeros a strings ya que si intento agregar numeros los vas a sumar en lugar de agregarlos al costado
     };
 
     // chooseOperation() define lo que ocurre cada vez que se hace click en una operacion
     chooseOperation(operation) {
-        
+        if (this.currentOperand === "") return; // Si currentOperand esta vacio, no nos va a dejar seguir
+        if (this.previousOperand !== "") { // Si previousOperand no esta vacio, se realiza la computacion
+            this.compute();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand; // previousOperand pasa a ser currentOperand...
+        this.currentOperand = ""; // ...y currentOperand queda vacio, indicando que es una nueva operacion
     };
 
     // compute() toma los valores de la calculadora y calcula lo que se va a mostrar en el display
-    compute() {};
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand); // parseFloat() convierte el string de vuelta a numero
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return; // Si no tengo prev y no tengo current, la funcion no se ejecuta
+        switch (this.operation) { // Defino un switch con todos los casos de operaciones posibles
+            case "+":
+                computation = prev + current;
+                break;
+            case "-":
+                computation = prev - current;
+                break;
+            case "*":
+                computation = prev * current;
+                break;
+            case "÷":
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
+    };
 
     // updateDisplay() actualiza el display segun los resultados de compute()
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand; // Esto mueve el valor de currentOperand a previousOperand
     };
 };
 
@@ -50,15 +81,26 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 // Selecciono todos los botones numericos y por cada uno le agrego un eventListener para que ocurra algo cuando le doy click
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        calculator.appendNumber(button.innerText) // Le paso el contenido de los botones (en este caso, los numeros) a la funcion appendNumber() de la clase
-        calculator.updateDisplay() // Y luego actualizo el display para que se muestre lo que clickeo
-    })
+        calculator.appendNumber(button.innerText); // Le paso el contenido de los botones (en este caso, los numeros) a la funcion appendNumber() de la clase
+        calculator.updateDisplay(); // Y luego actualizo el display para que se muestre lo que clickeo
+    });
 });
 
 // Hago lo mismo para las operaciones
 operationButtons.forEach(button => {
     button.addEventListener("click", () => {
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
-    })
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+// Le doy la funcion al "=" para que al hacerle click, realice la operacion y actualize el display
+equalsButton.addEventListener("click", button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", button => {
+    calculator.clear();
+    calculator.updateDisplay();
 });
